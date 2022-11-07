0a5f4471d4159752f126bbbb997b475b0bd3e9ee
sapiens12 <sapiens12@nate.com>
2022-11-07 06:58:47 +0100

    commit

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import App from './App';
 import NotFound from './pages/NotFound';
-import Login from './components/login/Login';
+import Login from './pages/Login';
 import Record from './pages/Record';
 
 const router = createBrowserRouter([
@@ -12,12 +12,12 @@ const router = createBrowserRouter([
     path: '/',
     element: <App />,
     errorElement: <NotFound />,
-    // children: [
-    //   { index: true, element: <Login /> },
-    //   { path: 'record', element: <Record /> },
-    //   { path: 'user01', element: <Login userId='nfc01@nfc.com' userPw='wearecyborgs'/> },
-    //   { path: 'user02', element: <Login userId='nfc02@nfc.com' userPw='wearecyborgs'/> },
-    // ],
+    children: [
+      { index: true, element: <Login /> },
+      { path: 'record', element: <Record /> },
+      { path: 'user01', element: <Login userId='nfc01@nfc.com' userPw='wearecyborgs'/> },
+      { path: 'user02', element: <Login userId='nfc02@nfc.com' userPw='wearecyborgs'/> },
+    ],
   },
 ]);
 const root = ReactDOM.createRoot(document.getElementById('root'));
